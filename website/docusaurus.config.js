85b879ddcd074e12a3cd0a30923e44a1cdab1ea9
Naffy Dharni <naffydharni006@gmail.com>
2022-09-06 07:17:32 +0000

    Update Navbar Brand Title

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Nextron',
+        title: 'NEXTRON',
         items: [
           {
             type: 'doc',
